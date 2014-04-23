afa3d740126aedef93ccfadb0d03b188a51bd8d7
shichunxiang0 <shichunxiang0@sina.cn>
2014-04-23 13:23:52 +0800

    initial
    The first commit .This is a try.

---
+++ CodeTools/src/com/shichunxiang/codetools/net/DeviceNetInfoUtil.java
@@ -0,0 +1,24 @@
+package com.shichunxiang.codetools.net;
+/**
+ * 
+ * @author shichunxiang
+ * 1.判断手机联网状态
+ * 2.获取手机IP地址
+ * 3.获取手机MAC地址
+ *
+ */
+public class DeviceNetInfoUtil {
+	
+	public static void getNetState(){
+		
+	}
+	public static String getDeviceIp(){
+		
+	}
+	public static String getDeviceMacAddress(){
+		
+	}
+	
+	
+
+}
