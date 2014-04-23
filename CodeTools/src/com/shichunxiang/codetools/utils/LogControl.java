33ea2b0687776f29dd0c74cdad84c68e3ff854d5
shichunxiang0 <shichunxiang0@sina.cn>
2014-04-23 16:00:18 +0800

    none

--- CodeTools/src/com/shichunxiang/codetools/utils/LogControl.java
+++ CodeTools/src/com/shichunxiang/codetools/utils/LogControl.java
@@ -1,7 +1,12 @@
 package com.shichunxiang.codetools.utils;
 
 import android.util.Log;
-
+/**
+ * 应用调试，flag=true，输出打印信息
+ * 应用上架，flag=false,进制打印信息输出，减少资源占用。
+ * @author shichunxiang
+ *
+ */
 public class LogControl {
 	public static boolean flag=true;
 	public static  void i(String tag,String msg){
